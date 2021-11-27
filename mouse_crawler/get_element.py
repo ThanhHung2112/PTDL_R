64262698c5b70b0d9e702b0b97ca74e2df3aa560
Lê Thanh Hùng <73764342+ThanhHung2112@users.noreply.github.com>
2021-11-27 22:58:58 +0700

    wait till it in a class

--- mouse_crawler/get_element.py
+++ mouse_crawler/get_element.py
@@ -1,45 +1,75 @@
 from selenium import webdriver
-import random
-from selenium.webdriver.common.keys import Keys
-from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 import pandas as pd
+
+
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--window-size=1920x1080")
 
 df = pd.read_csv("mouse_link.csv")
 n = 1
-for link in df[0]:
+
+for link in df["m_link"]:
 
     browser = webdriver.Chrome(executable_path="chromedriver.exe")
     browser.get(str(link))
-    sleep(random.randint(1, 4))
     #browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    browser.execute_script("window.scrollTo(0, 2350)")
-    sleep(random.randint(4, 5))
-
-    name = browser.find_element_by_class_name("title")
-    print(n,name.text)
+    try:
+        name = WebDriverWait(browser,10).until(
+            EC.presence_of_element_located((By.CLASS_NAME,"title"))
+        )
+    finally:
+        print(n,name.text)
     print("link:", link)
-    price = browser.find_element_by_class_name("product-price__current-price") #flash-sale-price
-    print(price.text)
-    if len(browser.find_elements_by_xpath("/html/body/div[1]/div[1]/main/div[3]/div[1]/div[3]/div[1]/div[2]/div[2]")) > 0:
+
+    element_exist = False if len(browser.find_elements_by_class_name('product-price__current-price')) > 0 else True
+    if element_exist == False:
+        price = browser.find_element_by_class_name("product-price__current-price") #flash-sale-price
+        sale = "Non"
+    else:
+        price = browser.find_element_by_class_name("flash-sale-price")
+        sale = "sale"
+    print("Price:   ",price.text)
+    print("Sale:",sale)
+
+    browser.execute_script("window.scrollTo(0, 2350)")
+
+    element_exist = False if len(browser.find_elements_by_xpath('/html/body/div[1]/div[1]/main/div[3]/div[1]/div[3]/div[1]/div[2]/div[2]')) > 0 else True
+    if element_exist == False:
         sold = browser.find_element_by_xpath("/html/body/div[1]/div[1]/main/div[3]/div[1]/div[3]/div[1]/div[2]/div[2]")
-    else: sold = 0
-    print(sold.text)
+        print(sold.text)
+        try:
+            rate = WebDriverWait(browser, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "review-rating__point"))
+            )
+            review = browser.find_element_by_class_name('review-rating__total')
+            star = browser.find_elements_by_class_name("review-rating__number")
 
-    rate = browser.find_element_by_class_name("review-rating__point")
-    print("Rate :", rate.text)
-    review = browser.find_element_by_class_name('review-rating__total')
-    print(review.text)
+        finally:
+            print("Rate :", rate.text)
+            print(review.text)
+            star5 = star[0].text
+            star4 = star[1].text
+            star3 = star[2].text
+            star2 = star[3].text
+            star1 = star[4].text
+    else:
+        sold = 0
+        print(sold)
+        rate = 0
+        print(rate)
+        review = 0
+        star5 = star4 = star3 = star2 = star1 = 0
 
-    star = browser.find_elements_by_class_name("review-rating__number")
-    star5 = star[0].text
-    star4 = star[1].text
-    star3 = star[2].text
-    star2 = star[3].text
-    star1 = star[4].text
 
+    #rate = browser.find_element_by_class_name("review-rating__point")
+    #print("Rate :", rate.text)
 
     def rate_star(star1, star2, star3, star4, star5):
         print("5 star :", star5, "\n"
--- mouse_crawler/get_mouse_link.py
+++ mouse_crawler/get_mouse_link.py
@@ -2,42 +2,83 @@ from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 import random
 import os
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 from selenium.webdriver.chrome.options import Options
 import pandas as pd
 
 # Options
 
+
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--incognito")
-chrome_options.add_argument("--window-size=1920x1080")
+#chrome_options.add_argument("--window-size=1920x1080")
 
-browser = webdriver.Chrome(executable_path="chromedriver.exe")
-browser.get("https://tiki.vn/search?q=chu%E1%BB%99t+m%C3%A1y+t%C3%ADnh&sort=top_seller&page=1")
-sleep(random.randint(2,5))
 
-html = browser.find_element_by_tag_name('html')
-for i in range(5):
-    html.send_keys(Keys.PAGE_DOWN)
+def star_url(i):
+    browser = webdriver.Chrome(executable_path="chromedriver.exe")
+    browser.maximize_window()
+    browser.get("https://tiki.vn/search?q=chu%E1%BB%99t+m%C3%A1y+t%C3%ADnh&sort=top_seller&page="+str(i))
+    return
+
+'''
+chrome_options.add_argument("--start-maximized")
+browser = c
+browser = ChromeDriver(options)
+'''
+page_number = int(input("Write page number to craw: "))
+
+while  page_number > 10 :
+    print("page_number p must be smaller than 10    ")
+    page_number = int(input("Write page number to craw:"))
+def page_up():
+    html = browser.find_element_by_tag_name('html')
+    for i in range(5):
+        html.send_keys(Keys.PAGE_UP)
+    sleep(2)
+    return
 
 mouse_link = []
 
-all_mouse = browser.find_elements_by_class_name("info")
+#all_mouse = browser.find_elements_by_class_name("info")
 x = a = 1
-for mouse in all_mouse :
-    #print(x, mouse.text)
-    #Get Link
-    links = browser.find_elements_by_class_name("product-item")
-    for link in links:
-        z = link.get_attribute("href")
-        mouse_link.append(z)
-        print(a,z)
-        a = a+1
-    x = x+1
-browser.close()
+for i in range(page_number):
+    browser = webdriver.Chrome(executable_path="chromedriver.exe")
+    browser.maximize_window()
+    browser.get("https://tiki.vn/search?q=chu%E1%BB%99t+m%C3%A1y+t%C3%ADnh&sort=top_seller&page=" + str(i+1))
+
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    try:
+        links = WebDriverWait(browser,10).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME,"product-item")))
+
+    finally:
+        print("page:",i + 1)
+
+        for link in links:
+            print(link)
+            z = link.get_attribute("href")
+            mouse_link.append(z)
+            print(a,z)
+            a = a+1
+    print(page_up())
+    browser.close()
+
+'''    element_wait = False if len(browser.find_elements_by_xpath(
+        '/html/body/div[1]/div[1]/main/div[2]/div/div[2]/div[1]/div[3]/ul/li[6]/a/i')) > 0 else True
+    while element_wait == True:
+        element_wait = False if len(browser.find_elements_by_xpath(
+            '/html/body/div[1]/div[1]/main/div[2]/div/div[2]/div[1]/div[3]/ul/li[6]/a/i')) > 0 else True
+    arrow = browser.find_element_by_xpath("/html/body/div[1]/div[1]/main/div[2]/div/div[2]/div[1]/div[3]/ul/li[6]/a/i")
+    arrow.click()
+    sleep(random.randint(1,4))'''
+
 df = pd.DataFrame(mouse_link)
+print(df)
 print(".....Đã craw hoàn tất",a - 1,"link..........")
 print("Press s to stop program","\n"
       "Press c to countinute craw data","\n"
@@ -63,7 +104,7 @@ def create_file(df):
     ask = input("Wanna create a csv file with this dataframe ?(y/n)")
     if ask == "y":
         f_name = input("file name:")
-        df.to_csv("C:\Users\Admin\Desktop\Web Scrapy\mouse_crawler\mouse_link.csv)
+        df.to_csv("C:\\Users\\Admin\\Desktop\\Web Scrapy\\mouse_crawler\\mouse24link.csv")
     return
 
 print(which_next(check,countinute))
